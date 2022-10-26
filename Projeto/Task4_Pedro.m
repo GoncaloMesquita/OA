c4adfa2270043f43b42378551c83335ddaa9cac5
pedroafonso92 <41843204+pedroafonso92@users.noreply.github.com>
2022-10-26 20:26:59 +0100

    Updates on code
    Task 4 and Task 7

--- Projeto/Task4_Pedro.m
+++ Projeto/Task4_Pedro.m
@@ -2,7 +2,7 @@ clear all
 
 %variables
 T = 60; % time steps
-y = 0.5;
+lambda = 0.5; %lambda value
 x_initial = [1;1;0;0];
 q1 = importdata('target_1.mat');
 q2 = importdata('target_2.mat');
@@ -22,7 +22,7 @@ for j=1:length(p1)
         variable x(4,T);
         variable u(2,(T-1));
 
-        % Compute TE_1, TE_2
+        % Compute TEs
         for i = 1:T
             TE1 = TE1 + max(abs(E*x(:,i) - q1(:,i)));
             TE2 = TE2 + max(abs(E*x(:,i) - q2(:,i)));
@@ -36,10 +36,12 @@ for j=1:length(p1)
             CE = CE + norm(u(:,i))^2;
         end
 
-        f = TE_avg + y * CE;
+        %optimization function
+        f = TE_avg + lambda * CE;
         minimize(f);
 
-        % subject t
+        % subject to
+        
         x(:,1) == x_initial;
         for i=2:(T)
             x(:,i) == A*x(:,i-1) + B*u(:,i-1);
@@ -48,16 +50,16 @@ for j=1:length(p1)
     cvx_end;
 
     %plot trajectory of target and vehicle
-    figure(j);
+    figure;
     hold on
-    plot(x(1,:),x(2,:),'.-k','MarkerSize',10,'LineWidth',1);
-    title(sprintf('Trajectories for p1 = %.2f and p2 = %.2f', p1(j),p2(j)));
-    plot(q1(1,:),q1(2,:),'.-r','MarkerSize',10,'LineWidth',1);
-    plot(q2(1,:),q2(2,:),'.-m','MarkerSize',10,'LineWidth',1);
+    plot(x(1,:),x(2,:),'.-k','MarkerSize',10,'LineWidth',1,'DisplayName',sprintf('Trajectory of our vehicle for p1 = %.1f and p2 = %.1f', p1(j),p2(j)));
+    plot(q1(1,:),q1(2,:),'.-r','MarkerSize',10,'LineWidth',1,'DisplayName','Target trajectory 1');
+    plot(q2(1,:),q2(2,:),'.-m','MarkerSize',10,'LineWidth',1,'DisplayName','Target trajectory 2');
     xlim([-1.5 1.5])
     ylim([-1.5 1.5])
     grid(gca,'minor')
     grid on
+    legend('Location','northwest')
     saveas(gcf,sprintf('Trajectories for p1 = %.2f and p2 = %.2f .png', p1(j),p2(j)))
 end
 
--- Projeto/Task7_Pedro.m
+++ Projeto/Task7_Pedro.m
@@ -2,9 +2,8 @@ clear all
 
 %variables
 T = 60; %time steps
-T_midway = 35;
-K = 2;  %number of trajectorys
-y = 0.05;%weigth
+tau = 35; %value of tau (midway time when target trajectory)
+lambda = 0.5;  %lambda value
 x_initial = [1;1;0;0];
 q1 = importdata('target_1.mat');
 q2 = importdata('target_2.mat');
@@ -26,22 +25,25 @@ cvx_begin quiet
     variable u1(2,(T-1));
     variable u2(2,(T-1));
 
-    % Compute 
+    % Compute TEs
     for i = 1:T
         TE1 = TE1 + max(abs(E*x1(:,i) - q1(:,i)));
         TE2 = TE2 + max(abs(E*x2(:,i) - q2(:,i)));
     end
 
-     % Compute CE
+     % Compute CEs
     for i = 1:(T-1)
         CE1 = CE1 + norm(u1(:,i))^2;
         CE2 = CE2 + norm(u2(:,i))^2;
     end
 
-    f = (p(1) * (TE1 + y * CE1)) + (p(2) * (TE2 + y * CE2));
+    %optimization function
+    f = (p(1) * (TE1 + lambda * CE1)) + (p(2) * (TE2 + lambda * CE2));
     minimize(f);
 
     % subject to
+    
+    %initial constrains
     x1(:,1) == x_initial;
     for i = 2:(T)
         x1(:,i) == A*x1(:,i-1) + B*u1(:,i-1);
@@ -52,25 +54,30 @@ cvx_begin quiet
         x2(:,i) == A*x2(:,i-1) + B*u2(:,i-1);
     end
     
-    for i = 1:(T_midway - 1)
+    %aditional contrains
+    for i = 2:(tau)
+        x1(:,i) == x2(:,i);
+    end
+    
+    for i = 1:(tau - 1)
         u1(:,i) == u2(:,i);
     end
-   
+    
 cvx_end;
 
 %plot trajectory of target and vehicle
 figure;
 hold on
-plot(x1(1,:),x1(2,:),'.-y','MarkerSize',10,'LineWidth',1);
-plot(x2(1,:),x2(2,:),'.-y','MarkerSize',10,'LineWidth',1);
-title(sprintf('Trajectories for p1 = %.2f and p2 = %.2f', p(1),p(2)));
-plot(q1(1,:),q1(2,:),'.-r','MarkerSize',10,'LineWidth',1);
-plot(q2(1,:),q2(2,:),'.-m','MarkerSize',10,'LineWidth',1);
+plot(x1(1,:),x1(2,:),'.-k','MarkerSize',10,'LineWidth',1,'DisplayName','Trajectory of our vehicle');
+plot(x2(1,:),x2(2,:),'.-k','MarkerSize',10,'LineWidth',1,'HandleVisibility','off');
+plot(q1(1,:),q1(2,:),'.-r','MarkerSize',10,'LineWidth',1,'DisplayName','Target trajectory 1');
+plot(q2(1,:),q2(2,:),'.-m','MarkerSize',10,'LineWidth',1,'DisplayName','Target trajectory 2');
 xlim([-1.5 1.5])
 ylim([-1.5 1.5])
 grid(gca,'minor')
 grid on
-%saveas(gcf,sprintf('Trajectories for p1 = %.2f and p2 = %.2f .png', p(1),p(2)))
+legend('Location','northwest')
+saveas(gcf,'Task7\Result_task7.png')
 
 
 
